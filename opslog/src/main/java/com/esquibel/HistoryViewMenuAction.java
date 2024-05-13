27fe0c4dafe0c2b1f57cfef6d348c11d94180c11
SpaceMan <153625573+91besquibel@users.noreply.github.com>
2024-05-13 15:38:38 -0600

    Update HistoryViewMenuAction.java

--- opslog/src/main/java/com/esquibel/HistoryViewMenuAction.java
+++ opslog/src/main/java/com/esquibel/HistoryViewMenuAction.java
@@ -2,7 +2,7 @@ public class HistoryViewMenuAction implements MenuAction {
     @Override
     public void execute() {
         // Logic for Setting menu action
-        PopupWindow settingPopup = new PopupWindow("History View");
+        PopupWindow historyPopup = new PopupWindow("History View");
         historyViewPopup.show();
     }
 }
