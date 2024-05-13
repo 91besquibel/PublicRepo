df814f956cb71c729fb3d6c9342f29fb8eb93c52
SpaceMan <153625573+91besquibel@users.noreply.github.com>
2024-05-13 08:26:32 -0600

    Update MainApp.java

--- opslog/src/main/java/com/esquibel/opslog/App.java
+++ opslog/src/main/java/com/esquibel/opslog/App.java
@@ -1,1 +1,27 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        TabPane tabPane = new TabPane();
+        tabPane.getTabs().add(new CalendarTab());
+        tabPane.getTabs().add(new OpsLogTab());
+
+        BorderPane root = new BorderPane();
+        root.setCenter(tabPane);
+
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Calendar Example");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
