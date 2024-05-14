d53d95c6835f06b5f6ef2e08907b3b31f9badff4
91besquibel <91besquibel@gmail.com>
2024-05-14 06:34:41 -0600

    update

--- opslog/src/main/java/com/esquibel/opslog/App.java
+++ opslog/src/main/java/com/esquibel/opslog/App.java
@@ -2,40 +2,42 @@ package com.esquibel.opslog;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
+    private TabPane tabPane;
+
     @Override
     public void start(Stage primaryStage) {
-        // Create menu bar
-        MenuBar menuBar = new MenuBar();
-        Menu menuSetting = new Menu("Setting");
-        Menu menuTabManager = new Menu("Tab Manager");
-        Menu menuHistoryView = new Menu("History View");
-        Menu menuHelp = new Menu("Help");
-        menuBar.getMenus().addAll(menuSetting, menuTabManager, menuHistoryView, menuHelp);
+        // Create buttons
+        Button settingButton = new Button("Setting");
+        Button tabManagerButton = new Button("Tab Manager");
+        Button historyViewButton = new Button("History View");
+        Button helpButton = new Button("Help");
+
+        // Set event handlers for buttons
+        settingButton.setOnAction(event -> executeMenuAction(new SettingMenuAction()));
+        tabManagerButton.setOnAction(event -> executeMenuAction(new TabManagerMenuAction(tabPane)));
+        historyViewButton.setOnAction(event -> executeMenuAction(new HistoryViewMenuAction()));
+        helpButton.setOnAction(event -> executeMenuAction(new HelpMenuAction()));
 
         // Create tab pane and add a welcome tab
-        TabPane tabPane = new TabPane();
+        tabPane = new TabPane();
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE); // Disable tab closing
-        Tab welcomeTab = new Tab("Welcome");
         VBox welcomeContent = new VBox(new Label("Welcome to the App!"));
-        welcomeTab.setContent(welcomeContent);
-        tabPane.getTabs().add(welcomeTab);
-
-        // Set event handlers for menu items
-        menuSetting.setOnAction(event -> executeMenuAction(new SettingMenuAction()));
-        menuTabManager.setOnAction(event -> executeMenuAction(new TabManagerMenuAction(tabPane)));
-        menuHistoryView.setOnAction(event -> executeMenuAction(new HistoryViewMenuAction()));
-        menuHelp.setOnAction(event -> executeMenuAction(new HelpMenuAction()));
+        tabPane.getTabs().add(new Tab("Welcome", welcomeContent));
 
         // Create border pane to arrange components
         BorderPane root = new BorderPane();
-        root.setTop(menuBar);
+        VBox buttonBox = new VBox(settingButton, tabManagerButton, historyViewButton, helpButton);
+        root.setLeft(buttonBox);
         root.setCenter(tabPane);
 
         Scene scene = new Scene(root, 800, 600);
@@ -46,6 +48,7 @@ public class App extends Application {
     }
 
     private void executeMenuAction(MenuAction action) {
+        System.out.println("Menu item clicked: " + action);
         action.execute();
     }
 
